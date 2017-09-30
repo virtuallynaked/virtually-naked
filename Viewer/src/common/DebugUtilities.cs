07ebeeed66f1132907adc6840872d9d8ba9ee688
Greg <virtuallynakedvr@gmail.com>
2017-09-30 12:10:15 -0700

    Add debug utilities to sleep-wait and busy-wait for a given duration

---
+++ Viewer/src/common/DebugUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+static class DebugUtilities {
+	public static void Burn(long ms) {
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		while (stopwatch.ElapsedMilliseconds < ms) {
+		}
+	}
+
+	public static void Sleep(long ms) {
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		while (stopwatch.ElapsedMilliseconds < ms) {
+			Thread.Yield();
+		}
+	}
+}
+
