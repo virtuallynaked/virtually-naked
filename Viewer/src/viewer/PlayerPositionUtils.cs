5d853930256faacfb294a811d49a3bf91ab774db
Greg <virtuallynakedvr@gmail.com>
2017-10-12 10:03:14 -0700

    Fine tune head position

--- Viewer/src/viewer/PlayerPositionUtils.cs
+++ Viewer/src/viewer/PlayerPositionUtils.cs
@@ -4,7 +4,10 @@ using Valve.VR;
 public static class PlayerPositionUtils {
 	public static Vector3 GetHeadPosition(TrackedDevicePose_t hmdPose) {
 		Matrix hmdToWorldTransform = hmdPose.mDeviceToAbsoluteTracking.Convert();
-		Vector3 headPositionInHmdSpace = new Vector3(0, -0.01f, +0.05f);
+
+		//The HMD position is midway between the lenses, so I need to go back and down a bit to the to the center of the player's face
+		Vector3 headPositionInHmdSpace = new Vector3(0, -0.01f, +0.04f);
+
 		Vector3 headPositionInWorldSpace = Vector3.TransformCoordinate(headPositionInHmdSpace, hmdToWorldTransform);
 		return headPositionInWorldSpace;
 	}
