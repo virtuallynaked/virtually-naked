dcafe0f27d0283279af5cc47c0b6e4076d9ce85b
Greg <virtuallynakedvr@gmail.com>
2017-10-24 19:06:44 -0700

    Add simple IK solving to FlatIkApp

--- Demos/src/FlatIk/FlatIkApp.cs
+++ Demos/src/FlatIk/FlatIkApp.cs
@@ -45,8 +45,54 @@ namespace FlatIk {
 				Vector2 worldPosition = Matrix3x2.TransformPoint(transform, formPosition);
 				target = worldPosition;
 			};
-			
+
+			renderEnvironment.Form.KeyPress += (sender, e) => {
+				if (e.KeyChar == ' ') {
+					DoIkIteration();
+				}
+			};
+
 			renderEnvironment.Run(Render);
+		}
+
+		private float Atan(Vector2 v) {
+			return (float) Math.Atan2(v.Y, v.X);
+		}
+
+		private float AngleDelta(Vector2 from, Vector2 to) {
+			float angleDelta = Atan(to) - Atan(from);
+
+			while (angleDelta < -MathUtil.Pi)
+            {
+                angleDelta += MathUtil.TwoPi;
+            }
+			while (angleDelta > +MathUtil.Pi)
+            {
+                angleDelta -= MathUtil.TwoPi;
+            }
+			
+			return angleDelta;
+		}
+
+		private void AdjustBone(Bone bone, Vector2 source, float weight) {
+			var transform = bone.GetChainedTransform();
+			var center = Matrix3x2.TransformPoint(transform, bone.Center);
+			
+			float angleDelta = AngleDelta(source - center, target - center);
+
+			bone.Rotation += angleDelta * weight;
+		}
+
+		private void DoIkIteration() {
+			var sourceBone = bones[bones.Count - 1];
+			var source = Matrix3x2.TransformPoint(sourceBone.GetChainedTransform(), sourceBone.End);
+
+			float decay = 0.9f;
+			float weight = 1 - decay;
+			for (var bone = sourceBone; bone != null; bone = bone.Parent) {
+				AdjustBone(bone, source, weight);
+				weight *= decay;
+			}
 		}
 
 		private Matrix3x2 GetWorldToFormTransform() {
