e4d992c5845cd92b4716e6b4013701d781973f41
Greg <virtuallynakedvr@gmail.com>
2017-12-09 14:39:01 -0800

    Add ClampToEllipse utility method

---
+++ UnitTests/src/math/EllipseClampTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpDX;
+using System;
+
+[TestClass]
+public class EllipseClampTest {
+    private const float Acc = 1e-4f;
+
+    private void AssertClampedResult(float expectedX, float expectedY, float x, float y, float minX, float maxX, float minY, float maxY) {
+		EllipseClamp.ClampToEllipse(ref x, ref y, minX, maxX, minY, maxY);
+		Assert.AreEqual(expectedX, x, Acc);
+		Assert.AreEqual(expectedY, y, Acc);
+    }
+
+	private void AssertInside(float x, float y, float minX, float maxX, float minY, float maxY) {
+		AssertClampedResult(x, y, x, y, minX, maxX, minY, maxY);
+    }
+
+	[TestMethod]
+	public void TestInside() {
+		AssertInside( 0,  0, -2, +3, -4, +5);
+		AssertInside(-1,  0, -2, +3, -4, +5);
+		AssertInside(+1,  0, -2, +3, -4, +5);
+		AssertInside( 0, -1, -2, +3, -4, +5);
+		AssertInside( 0, +1, -2, +3, -4, +5);
+		AssertInside(-1, -1, -2, +3, -4, +5);
+		AssertInside(-1, +1, -2, +3, -4, +5);
+		AssertInside(+1, -1, -2, +3, -4, +5);
+		AssertInside(+1, +1, -2, +3, -4, +5);
+	}
+
+	[TestMethod]
+	public void TestOutsideOnAxis() {
+		AssertClampedResult(-2, 0, -10, 0, -2, +3, -4, +5);
+		AssertClampedResult(+3, 0, +10, 0, -2, +3, -4, +5);
+		AssertClampedResult(0, -4, 0, -10, -2, +3, -4, +5);
+		AssertClampedResult(0, +5, 0, +10, -2, +3, -4, +5);
+	}
+
+	[TestMethod]
+	public void TestUnitCircle() {
+		float sqrt2 = (float) Math.Sqrt(0.5);
+		AssertClampedResult(sqrt2, sqrt2, +10, +10, -1, +1, -1, +1);
+	}
+
+	[TestMethod]
+	public void TestOutside() {
+		//expected values from Mathematica's NMinimize
+		AssertClampedResult(-1.10904f, -3.32868f, -10, -10, -2, +3, -4, +5);
+		AssertClampedResult(-1.05603f, +4.24618f, -10, +10, -2, +3, -4, +5);
+		AssertClampedResult(+1.9619f, -3.02609f, +10, -10, -2, +3, -4, +5);
+		AssertClampedResult(+1.8739f, +3.90459f, +10, +10, -2, +3, -4, +5);
+	}
+
+	[TestMethod]
+	public void TestDegenerateToPoint() {
+		//degenerate to point
+		AssertClampedResult(0, 0, -10, -10, 0, 0, 0, 0);
+		AssertClampedResult(0, 0, -10, +10, 0, 0, 0, 0);
+		AssertClampedResult(0, 0, +10, -10, 0, 0, 0, 0);
+		AssertClampedResult(0, 0, +10, +10, 0, 0, 0, 0);
+	}
+
+	[TestMethod]
+	public void TestDegenerateToLineSegment() {
+		//degenerate to line segment on x-axis
+		AssertClampedResult(-2, 0, -10, -10, -2, +3, 0, 0);
+		AssertClampedResult( 0, 0,   0, -10, -2, +3, 0, 0);
+		AssertClampedResult(+3, 0, +10, -10, -2, +3, 0, 0);
+		AssertClampedResult(-2, 0, -10, +10, -2, +3, 0, 0);
+		AssertClampedResult( 0, 0,   0, +10, -2, +3, 0, 0);
+		AssertClampedResult(+3, 0, +10, +10, -2, +3, 0, 0);
+
+		//degenerate to line segment on y-axis
+		AssertClampedResult(0, -4, -10, -10, 0, 0, -4, +5);
+		AssertClampedResult(0,  0, -10,   0, 0, 0, -4, +5);
+		AssertClampedResult(0, +5, -10, +10, 0, 0, -4, +5);
+		AssertClampedResult(0, -4, +10, -10, 0, 0, -4, +5);
+		AssertClampedResult(0,  0, +10,   0, 0, 0, -4, +5);
+		AssertClampedResult(0, +5, +10, +10, 0, 0, -4, +5);
+	}
+}
---
+++ Viewer/src/math/EllipseClamp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using static MathExtensions;
+
+public static class EllipseClamp {
+	private const int MaxIter = 3;
+	
+	private static void Normalize(ref double x, ref double y) {
+		double m = Math.Sqrt(1 / (Sqr(x) + Sqr(y)));
+		x *= m;
+		y *= m;
+	}
+
+	private static void ClosestPointOnEllipse(double a, double b, double gx, double gy, out double x, out double y) {
+		double cos = b * gx;
+		double sin = a * gy;
+		Normalize(ref cos, ref sin);
+
+		for (int i = 0; i < MaxIter; ++i) {
+			double f = b*cos*gy + a*a*cos*sin - b*b*cos*sin - a*gx*sin;
+			double fdx = (a*a - b*b)*(cos*cos - sin*sin) - a*cos*gx - b*gy*sin;
+			double step = -f / fdx;
+			
+			double oldCos = cos;
+			cos -= step * sin;
+			sin += step * oldCos;
+			Normalize(ref cos, ref sin);
+		}
+
+		x = a * cos;
+		y = b * sin;
+	}
+
+	private static void ClampToEllipseFirstQuadrant(ref float x, ref float y, float limitX, float limitY) {
+		if (limitX <= 0 && limitY <= 0) {
+			x = 0;
+			y = 0;
+		} else if (limitX <= 0) {
+			x = 0;
+			y = Math.Min(y, limitY);
+		} else if (limitY <= 0) {
+			x = Math.Min(x, limitX);
+			y = 0;
+		} else {
+			float scaledNorm = MathExtensions.Sqr(x / limitX) + MathExtensions.Sqr(y / limitY);
+			if (scaledNorm <= 1) {
+				//inside of ellipse
+				return;
+			}
+
+			ClosestPointOnEllipse(limitX, limitY, x, y, out double clampedX, out double clampedY);
+			x = (float) clampedX;
+			y = (float) clampedY;
+		}
+	}
+
+	public static void ClampToEllipse(ref float x, ref float y, float minX, float maxX, float minY, float maxY) {
+		float absX, limitX;
+		if (x >= 0) {
+			limitX = maxX;
+			absX = x;
+		} else {
+			absX = -x;
+			limitX = -minX;
+		}
+
+		float absY, limitY;
+		if (y >= 0) {
+			limitY = maxY;
+			absY = y;
+		} else {
+			absY = -y;
+			limitY = -minY;
+		}
+
+		ClampToEllipseFirstQuadrant(ref absX, ref absY, limitX, limitY);
+
+		x = Math.Sign(x) * absX;
+		y = Math.Sign(y) * absY;
+	}
+}
