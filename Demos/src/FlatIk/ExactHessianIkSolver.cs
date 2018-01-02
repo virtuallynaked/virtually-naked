c13ba3f22cbadfc2b94f5aac5687d4b8b298c5f3
Greg <virtuallynakedvr@gmail.com>
2018-01-01 20:44:00 -0800

    Add a couple of experimental FlatIk solvers
    ExactHessian: Newton's method using the pseudo-inverse of the exact
    hessian.
    
    OverdefinedGaussNewtonIkSolver: Gauss-Newton but with additional goals of keeping each bone's center in the same location.

---
+++ Demos/src/FlatIk/ExactHessianIkSolver.cs
@@ -0,0 +1,63 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using SharpDX;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlatIk {
+	public class ExactHessianIkSolver : IIkSolver {
+		private readonly List<Bone> bones;
+
+		public ExactHessianIkSolver(List<Bone> bones) {
+			this.bones = bones;
+		}
+
+		public void DoIteration(SkeletonInputs inputs, Bone sourceBone, Vector2 unposedSource, Vector2 target) {
+			Vector2 posedSource = Matrix3x2.TransformPoint(sourceBone.GetChainedTransform(inputs), unposedSource);
+			List<Vector2> posedCenters = bones
+				.Select(bone => Matrix3x2.TransformPoint(bone.GetChainedTransform(inputs), bone.Center))
+				.ToList();
+			
+			List<Vector2> boneVectors = posedCenters
+				.Zip(posedCenters.Skip(1).Concat(new [] {posedSource}), (center, end) => end - center)
+				.ToList();
+
+			List<float> weights = Enumerable.Range(0, bones.Count).Select(i => 1f / (bones.Count - i)).ToList();
+
+			Vector<float> gradient = Vector<float>.Build.Dense(bones.Count);
+			Matrix<float> hessian = Matrix<float>.Build.Dense(bones.Count, bones.Count);
+
+			for (int i = 0; i < bones.Count; ++i) {
+				Vector2 bi = boneVectors[i];
+
+				Vector2 temp = target - posedSource + bi;
+
+				gradient[i] = weights[i] * (bi.Y * temp.X - bi.X * temp.Y);
+				hessian[i, i] = weights[i] * weights[i] * Vector2.Dot(bi, temp);
+
+				for (int j = 0; j < bones.Count; ++j) {
+					Vector2 bj = boneVectors[j];
+
+					if (i != j) {
+						hessian[i, j] = weights[i] * weights[j] * Vector2.Dot(bi, bj);
+					}
+				}
+			}
+			
+			//var step = -hessian.Inverse().Multiply(gradient);
+			var step = -hessian.PseudoInverse().Multiply(gradient);
+			//var step = -0.5f * gradient;
+
+			/*
+			// Ensure step is approaching a minimum and not a maximum
+			for (int i = 0; i < bones.Count; ++i) {
+				step[i] = -Math.Abs(step[i]) * Math.Sign(gradient[i]);
+			}
+			*/
+			
+			for (int i = 0; i < bones.Count; ++i) {
+				float localRotationDelta = step[i] - ((i > 0) ? step[i - 1] : 0);
+				inputs.IncrementRotation(i, weights[i] * localRotationDelta);
+			}
+		}
+	}
+}
---
+++ Demos/src/FlatIk/OverdefinedGaussNewtonIkSolver.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using SharpDX;
+using MathNet.Numerics.LinearAlgebra;
+using System.Linq;
+
+namespace FlatIk {
+	public class OverdefinedGaussNewtonIkSolver : IIkSolver {
+		private const float BoneCenterWeight = 1f;
+		private const float IkTargetWeight = 10f;
+
+		private static IEnumerable<Bone> GetBoneChain(Bone sourceBone) {
+			for (var bone = sourceBone; bone != null; bone = bone.Parent) {
+				yield return bone;
+			}
+		}
+
+		public void DoIteration(SkeletonInputs inputs, Bone sourceBone, Vector2 unposedSource, Vector2 target) {
+			Vector2 source = Matrix3x2.TransformPoint(sourceBone.GetChainedTransform(inputs), sourceBone.End);
+
+			List<Bone> bones = GetBoneChain(sourceBone).ToList();
+			int boneCount = bones.Count;
+			
+			Vector<float> residuals = Vector<float>.Build.Dense(boneCount * 2 + 2);
+			for (int boneIdx = 0; boneIdx < boneCount; ++boneIdx) {
+				var bone = bones[boneIdx];
+				var posedCenter = Matrix3x2.TransformPoint(bone.GetChainedTransform(inputs), bone.Center);
+				
+				var residual = bone.Center - posedCenter;
+				//var residual = Vector2.Zero;
+				residuals[boneIdx * 2 + 0] = BoneCenterWeight * residual.X;
+				residuals[boneIdx * 2 + 1] = BoneCenterWeight * residual.Y;
+			}
+			residuals[boneCount * 2 + 0] = IkTargetWeight * (target.X - source.X);
+			residuals[boneCount * 2 + 1] = IkTargetWeight * (target.Y - source.Y);
+
+			Matrix<float> jacobian = Matrix<float>.Build.Dense(2 * boneCount + 2, boneCount);
+			
+			for (int boneIdx = 0; boneIdx < boneCount; ++boneIdx) {
+				for (int targetIdx = 0; targetIdx < boneCount + 1; ++targetIdx) {
+					Vector2 boneGradient;
+					if (targetIdx > boneIdx && targetIdx != boneCount) {
+						//target is unaffected by this bone
+						boneGradient = Vector2.Zero;
+					} else {
+						Vector2 targetSource;
+						float weight;
+
+						if (targetIdx < boneCount) {
+							//target is a bone center
+							var targetBone = bones[targetIdx];
+							targetSource = Matrix3x2.TransformPoint(targetBone.GetChainedTransform(inputs), targetBone.Center);
+							weight = BoneCenterWeight;
+
+						} else {
+							//target is IK target
+							targetSource = source;
+							weight = IkTargetWeight;
+						}
+						boneGradient = weight * bones[boneIdx].GetGradientOfTransformedPointWithRespectToRotation(inputs, targetSource);
+					} 
+					
+					jacobian[targetIdx * 2 + 0, boneIdx] = boneGradient.X;
+					jacobian[targetIdx * 2 + 1, boneIdx] = boneGradient.Y;
+				}
+			}
+			Vector<float> step = jacobian.PseudoInverse().Multiply(residuals);
+			
+			for (int boneIdx = 0; boneIdx < boneCount; ++boneIdx) {
+				var bone = bones[boneIdx];
+				bone.IncrementRotation(inputs, step[boneIdx]);
+			}
+		}
+	}
+}
