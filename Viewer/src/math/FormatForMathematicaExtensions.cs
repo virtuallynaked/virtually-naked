1b688fbed5273ab24fbb4bd271182a9e826e70ad
Greg <virtuallynakedvr@gmail.com>
2018-03-05 19:52:37 -0800

    Add FormatForMathematicaExtensions extension class

---
+++ Viewer/src/math/FormatForMathematicaExtensions.cs
@@ -0,0 +1,11 @@
+﻿using SharpDX;
+
+public static class FormatForMathematicaExtensions {
+	public static string FormatForMathematica(this Vector3 v) {
+		return "{" + v.X + ", " + v.Y + ", " + v.Z + "}";
+	}
+
+	public static string FormatForMathematica(this Matrix3x3 m) {
+		return "{" + FormatForMathematica(m.Row1) + ", " + FormatForMathematica(m.Row2) + ", " + FormatForMathematica(m.Row3) + "}";
+	}
+}
