00be213964dba62ca9f772aa91532d6e35e4abbe
Greg <virtuallynakedvr@gmail.com>
2018-04-04 19:42:11 -0700

    Fine tune figure false depth
    The depth is biased inwards along the figure normal instead of directly
    away from the camera. Also, the bias is now 0.5 centimeters instead of
    2. This seems to strike a good balance between avoiding skin poking
    through clothing while also avoid the back surfaces of clothing poking
    through skin.

--- Viewer/src/figure/rendering/Figure-FalseDepth.vs.hlsl
+++ Viewer/src/figure/rendering/Figure-FalseDepth.vs.hlsl
@@ -1,5 +1,4 @@
 #include <d3d/VertexCommon.hlsl>
-#include <game/rendering/RenderingConstants.hlsl>
 #include "RefinedVertex.hlsl"
 
 cbuffer bar : register(b1) {
@@ -7,19 +6,21 @@ cbuffer bar : register(b1) {
 	float3x3 worldMatrixInverseTranspose;
 }
 
-static const float DepthBias = -0.02;
+static const float DepthBias = 0.5; //in centimeters
 
-float4 applyDepthBias(float4 screenPosition, float bias, float near, float far) {
-	float z = -screenPosition.w;
-	float zProjBiased = (far*(bias + near + z)) / ((far - near)*(bias + z));
-	float zScreenBiased = zProjBiased * screenPosition.w;
-
-	return float4(screenPosition.x, screenPosition.y, zScreenBiased, screenPosition.w);
-}
-
-float4 main(RefinedVertex vIn) : SV_POSITION{
-	float3 worldPosition = mul(float4(vIn.position, 1), worldMatrix).xyz;
+float4 main(RefinedVertex vIn) : SV_POSITION {
+	float3 position = vIn.position;
+	float3 worldPosition = mul(float4(position, 1), worldMatrix).xyz;
 	float4 screenPosition = calculateScreenPosition(worldPosition);
-	float4 biasedScreenPosition = applyDepthBias(screenPosition, DepthBias, znear, zfar);
-	return biasedScreenPosition;
+
+	float3 erodedPosition = vIn.position - DepthBias * vIn.normal;
+	float3 erodedWorldPosition = mul(float4(erodedPosition, 1), worldMatrix).xyz;
+	float4 erodedScreenPosition = calculateScreenPosition(erodedWorldPosition);
+	
+	float4 combinedScreenPosition = float4(
+		screenPosition.x,
+		screenPosition.y,
+		min(screenPosition.z, erodedScreenPosition.z / erodedScreenPosition.w * screenPosition.w),
+		screenPosition.w);
+	return combinedScreenPosition;
 }
